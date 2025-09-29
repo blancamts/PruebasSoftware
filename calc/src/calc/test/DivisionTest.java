94d875fc2ebfb62e2bc38c74f841d4ac7e656a80
Blanca Matas Gavira <168214008+blancamts@users.noreply.github.com>
2025-09-29 18:45:50 +0200

    Add unit tests for Division class

--- calc/src/calc/test/DivisionTest.java
+++ calc/src/calc/test/DivisionTest.java
@@ -1,5 +1,23 @@
 package calc.test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import calc.Division;
 
 public class DivisionTest {
+	
+	@Test
+	public void testDivision() {
+		Division division = new Division();
+		double resultado = division.getDivision(6.0, 3.0);
+		assertEquals(2, resultado, "Al dividir 6 por 3 debería dar 2");
+	}
+	
+	@Test
+	public void testDivide() 
+	{
+		Division division = new Division(6.0);
+		Double resultado = division.divide(3.0);
+		assertEquals(2, resultado, "Al dividir 6 por 3 debería dar 2");
+	}
 
 }
